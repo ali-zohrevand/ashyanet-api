a4b53351e03880f53a4abe22a7502cfe82fc7081
alizohrevand <alihooshyar1990@gmail.com>
2018-09-19 18:08:48 +0430

    Correct casbin mongo adaptor

--- Lab/role/casbin/02-mongo-adaptor/mongoCasbin.go
+++ Lab/role/casbin/02-mongo-adaptor/mongoCasbin.go
@@ -16,7 +16,7 @@ func main() {
 	// If it doesn't exist, the adapter will create it automatically.
 	// a := mongodbadapter.NewAdapter("127.0.0.1:27017/abc", true)
 
-	e := casbin.NewEnforcer("./auth_model.conf", a)
+	e := casbin.NewEnforcer("Lab/role/casbin/02-mongo-adaptor/auth_model.conf", a)
 	//p, alice, data1, read
 	// Load the policy from DB.
 	e.LoadPolicy()
