d636bf6461d4e87d955da37fe8dff3ae3bfe2234
gabriel <gabriel@it.sh>
2019-01-29 15:57:08 +0330

    add delete fun controller

--- controllers/types.go
+++ controllers/types.go
@@ -41,3 +41,21 @@ func TypesDelete(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) 
 	}
 
 }
+
+func TypesGetAll(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	typeObj := new(models.Types)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&typeObj)
+	userInDB, err := GetUserFromHeader(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("user not found"))
+
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		responseStatus, token := services.TypeGetAllTypes(userInDB)
+		w.WriteHeader(responseStatus)
+		w.Write(token)
+	}
+
+}
---
+++ routers/types.go
@@ -0,0 +1,30 @@
+package routers
+
+import (
+	"github.com/ali-zohrevand/ashyanet-api/controllers"
+	"github.com/ali-zohrevand/ashyanet-api/services"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func Types(router *mux.Router) *mux.Router {
+	HandleFuncTypesCreate := negroni.New(
+		negroni.HandlerFunc(services.RequireTokenAuthentication),
+		negroni.HandlerFunc(controllers.TypesCreate),
+	)
+	HandleFuncTypesGetAll := negroni.New(
+		negroni.HandlerFunc(services.RequireTokenAuthentication),
+		negroni.HandlerFunc(controllers.TypesGetAll),
+	)
+	HandleFuncTypesDelete := negroni.New(
+		negroni.HandlerFunc(services.RequireTokenAuthentication),
+		negroni.HandlerFunc(controllers.TypesDelete),
+	)
+	router.Handle("/user/types", HandleFuncTypesCreate).Methods("POST")
+	router.Handle("/user/types", HandleFuncTypesGetAll).Methods("GET")
+	router.Handle("/user/types", HandleFuncTypesDelete).Methods("DELETE")
+
+	//.......................................................................................................
+	return router
+
+}
